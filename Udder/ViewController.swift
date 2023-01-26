34fe68a79b7a580594b803c88a36c9d0b8bc3e76
Vladimir Alekseev <vovauu@icloud.com>
2023-01-26 13:11:30 +0300

    create viewDidDisappear func

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         //
     }
+    override func viewDidDisappear(_ animated: Bool) {
+        //
+    }
 
 }
 
